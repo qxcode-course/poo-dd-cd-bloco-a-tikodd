0355787fe8bbfa1322b32031a95c3622af7286e9
tikodd <tiagocm1085@gmail.com>
2025-09-22 20:14:28 +0000

    2h fazendo atividade da toalha

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,10 +1,22 @@
-class  Towel:
-    def __init__(self, color: str, size: str):
+class Towel:
+    def __init__(self, color: str, size: str): 
         self.color: str = color
         self.size: str = size
         self.wetness: int = 0
 
-    def getMaxWetness(self):
+    def dry(self, amount: int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print('toalha encharcada')
+            self.wetness = self.getMaxWetness()
+
+    def isDry(self) -> bool:
+        return self.wetness == 0
+        
+    def wringOut(self) -> None:
+        self.wetness = 0
+
+    def getMaxWetness(self) -> int:
         if self.size == 'P':
             return 10
         if self.size == 'M':
@@ -13,47 +25,26 @@ class  Towel:
             return 30
         return 0
         
-    def dry(self, amount: int):
-       self.wetness += amount
-       if self.wetness > self.getMaxWetness():
-           print ('toalha encharcada')
-           self.wetness = self.getMaxWetness()
+    def __str__(self) -> str:
+        return f"Cor:{self.color}, Tam:{self.size}, Umidade:{self.wetness}"
     
-        
-        
-
-    def wringOut(self):
-        self.wetness = 0
-
-    def isdry(self):
-        if self.wetness == 0:
-            print ('true')
+def main():
+    toalha = Towel("", "")
+    while True:
+        line: str = input()
+        args: list[str] = line.split("")
+        if args[0] == "end":
+            break
+        elif args[0] == "new":
+            color = args[1]
+            size = args[2]
+            toalha = Towel(color, size)
+        elif args[0] == "show":
+            print(toalha)
+        elif args[0] == "dry":
+            amount: int = int(args[1])
+            toalha.dry(amount)
         else:
-            print ('false')
-
-    def show(self):
-        print(self)
-
-    def __str__(self):
-        return f"{self.color} {self.size} {self.wetness}"
-
-towel = Towel("Azul", "P")
-towel.show()  # Azul P 0
-towel.dry(5)
-towel.show()  # Azul P 5
-print(towel.isDry()) # False
-towel.dry(5)
-towel.show()  # Azul P 10
-towel.dry(5) # msg: toalha encharcada
-towel.show()  # Azul P 10
-
-towel.wringOut()
-towel.show()  # Azul P 0
-
-towel = Towel("Verde", "G")
-print(towel.isDry()) # True
-towel.dry(30)
-towel.show()  # Verde G 30
-print(towel.isDry()) # False
-towel.dry(1)  # msg: toalha encharcada
-
+            print("fail: comando invalido")
+            
+main()
