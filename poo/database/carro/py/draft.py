ed60a3bb00a82e3843ffed8b7e2305f6c376b6f8
Tiago <tiagocm1085@gmail.com>
2025-10-04 23:07:35 -0300

    3h resolvendo atividade do carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,10 +1,26 @@
-class car:
-    def __init__(self, Pass: int, gas: int, km: int):
+class carro:
+    def __init__(self):
         self.Pass: int = 0
         self.gas: int = 0
         self.km: int = 0
 
     def __str__(self):
-        return f"pass:{self.Pass}, gas:{self.gas}, km:{self.km}"
+        return f"Pass:{self.Pass}, gas:{self.gas}, km:{self.km}"
+    
+    def show(self):
+        print(str(self))
+
+def main():
+    car = carro()
+    while True:
+        line = input()
+        if line == "end":
+            break
+
+        print("$" + line)
+        if line == "show":
+            car.show()
+
+main()
 
     
